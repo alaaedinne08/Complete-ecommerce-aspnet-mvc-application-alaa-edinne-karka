03d18e4955c2511f5c0eb203ec39c9025f081a47
aziz <aziz@DESKTOP-K184F5B>
2024-09-26 17:04:45 +0100

    Etickets

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data;
+
+namespace eTickets
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add serices to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //DbContextConfiguration
+            services.AddDbContext<AppDbContext>();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
