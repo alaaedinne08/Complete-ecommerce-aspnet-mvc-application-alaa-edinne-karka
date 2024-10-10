f8e3da060212747f04d61fb948c56180e283859b
aziz <aziz@DESKTOP-K184F5B>
2024-10-10 15:43:10 +0100

    Services

--- Program.cs
+++ Program.cs
@@ -1,39 +1,49 @@
 using eTickets.Data; // Import your Data context
+using eTickets.Data.Services;
+using FluentAssertions.Common;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container (equivalent to the ConfigureServices in Startup.cs)
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
-
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline (equivalent to the Configure method in Startup.cs)
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseDeveloperExceptionPage();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container (equivalent to the ConfigureServices in Startup.cs)
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+        //Services Configuration
+        builder.Services.AddScoped<IActorsService, ActorsService>();
+
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline (equivalent to the Configure method in Startup.cs)
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        // Seed the database
+        AppDbInitializer.Seed(app);
+
+        app.Run();
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Seed the database
-AppDbInitializer.Seed(app);
-
-app.Run();
